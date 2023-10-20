47d3ac36af6445164488d9d6514398b24c7ae43f
rohitkalampatil <rohitkalam96k@gmail.com>
2023-10-20 22:43:10 +0530

    Addedfiles

---
+++ coreJava/J01HelloWorld.java
@@ -0,0 +1,7 @@
+package coreJava;
+
+public class J01HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ coreJava/J02Variables.java
@@ -0,0 +1,12 @@
+package coreJava;
+
+import java.time.Clock;
+
+public class J02Variables {
+    public static void main(String[] args) {
+        int a=3;
+        String b = "Java Language";
+
+        System.out.println("a = "+a +"\nb = "+b);
+    }
+}
